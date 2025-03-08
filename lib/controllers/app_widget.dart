5b70ec947716b0eb4e3b643fb273b89579d33e67
Pedro Lima <pedrlimadev@gmail.com>
2025-03-08 18:02:12 -0300

    refactor: app widget

--- lib/controllers/app_widget.dart
+++ lib/controllers/app_widget.dart
@@ -3,25 +3,27 @@ import '../../pages/home_page.dart';
 import '../../pages/ticket_page.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flymigo',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      onGenerateRoute: (settings) {
-        if (settings.name == '/voo') {
-          final args = settings.arguments as Map<String, dynamic>?;
-
-          if (args != null && args.containsKey('travelId')) {
-            return MaterialPageRoute(
-              builder: (context) => TicketPage(travelId: args['travelId']),
-            );
-          }
-        }
-
-        return MaterialPageRoute(builder: (context) => HomePage());
-      },
+      onGenerateRoute: _generateRoute,
     );
+  }
+
+  Route<dynamic> _generateRoute(RouteSettings settings) {
+    if (settings.name == '/voo') {
+      final args = settings.arguments as Map<String, dynamic>?;
+      if (args?.containsKey('travelId') ?? false) {
+        return MaterialPageRoute(
+          builder: (context) => TicketPage(travelId: args!['travelId']),
+        );
+      }
+    }
+    return MaterialPageRoute(builder: (context) => HomePage());
   }
 }
